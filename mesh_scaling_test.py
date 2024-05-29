4829b46e5d3ea4d3e4a0ba783d7a5a3103f9c5d2
Shuda Li <lishuda1980@gmail.com>
2024-05-29 14:00:32 +0100

    add comments

--- mesh_scaling_test.py
+++ mesh_scaling_test.py
@@ -2,9 +2,11 @@ from csm import CSMClient
 
 csm_client = CSMClient(api_key='C6544b919C9DD82cccdF41d5D4778043')
 
-# a) using a local image path
-image_path = "/media/shuda/ssd2t/front_back/sheets/x512/29/0.jpg"
-
-mesh_path = csm_client.image_to_3d(image_path, mesh_format='obj', generate_spin_video = True, scaled_bbox=(0.8,0.8,0.8))
+#using a local image path
+image_path = "/media/shuda/ssd2t/front_back/sheets/x512/29/0.jpg" 
+#specify the width, height, and depth of the target object
+scaled_bbox = [0.86, 0.86, 0.1]    
+#generate preview mesh and download it.
+mesh_path = csm_client.image_to_3d(image_path, mesh_format='obj', generate_spin_video = True, scaled_bbox=scaled_bbox)
 
 print(f"Mesh path: {mesh_path}")
