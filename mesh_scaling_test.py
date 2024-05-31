e967c8ec7389b40c4b73719e81e335b858a38027
Shuda Li <lishuda1980@gmail.com>
2024-05-31 18:49:42 +0100

    house cleaning

--- mesh_scaling_test.py
+++ mesh_scaling_test.py
@@ -3,10 +3,10 @@ from csm import CSMClient
 csm_client = CSMClient(api_key='xxx')
 
 #using a local image path or image_url
-image_url = "/path/to/your/sofa.png"
+image_url = "./assets/sofa.png"
 #specify the width, height, and depth of the target object
-scaled_bbox = [0.86, 0.86, 0.1]    
+scaled_bbox = [0.86, 0.86, 0.5]    
 #generate preview mesh and download it.
-mesh_path = csm_client.image_to_3d(image_url, mesh_format='obj', generate_spin_video = False, scaled_bbox=scaled_bbox)
+mesh_path = csm_client.image_to_3d(image_url, mesh_format='obj', scaled_bbox=scaled_bbox)
 
 print(f"Mesh path: {mesh_path}")
